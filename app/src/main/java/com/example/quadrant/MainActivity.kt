c99bf9ce690ab05e96249acdedb2b7d72c7a16ec
Sarfaraz Shaikh <sarfarazshaikh.ss.1311@gmail.com>
2026-01-06 18:47:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quadrant/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.quadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quadrant.ui.theme.QuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuadrantTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    QuadrantPage()
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun QuadrantPage(modifier: Modifier = Modifier) {
+Column(Modifier.fillMaxWidth()){
+    Row(Modifier.weight(1f)) {
+        SingleQuadrant(
+            title = stringResource(R.string.text_composable),
+            description = stringResource(R.string.text_info),
+            backgroundColor = Color(0xFFEADDFF),
+            modifier = Modifier.weight(1f)
+        )
+
+        SingleQuadrant(
+            title = stringResource(R.string.image_composable),
+            description = stringResource(R.string.image_info),
+            backgroundColor = Color(0xFFD0BCFF),
+            modifier = Modifier.weight(1f)
+        )
+    }
+    Row(Modifier.weight(1f)) {
+        SingleQuadrant(
+            title = stringResource(R.string.row_composable),
+            description = stringResource(R.string.row_info),
+            backgroundColor = Color(0xFFB69DF8),
+            modifier = Modifier.weight(1f)
+        )
+
+        SingleQuadrant(
+            title = stringResource(R.string.column_composable),
+            description = stringResource(R.string.column_info),
+            backgroundColor = Color(0xFFF6EDFF),
+            modifier = Modifier.weight(1f)
+        )
+    }
+    }
+}
+
+
+@Composable
+private fun SingleQuadrant(title: String, description: String, backgroundColor: Color, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuadrantTheme {
+        QuadrantPage()
+    }
+}
